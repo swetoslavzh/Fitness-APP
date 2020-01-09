cde974133a392ebfd0ae062038bcad895b26f3e2
Svetoslav Zhleyazkov <swetoslavj@gmail.com>
2020-01-09 19:45:35 +0200

    Fixing response interceptor

--- public/src/app/core/interceptors/response-handler.interceptor.ts
+++ public/src/app/core/interceptors/response-handler.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material';
 
@@ -12,37 +12,41 @@ export class ResponseHandlerInterceptorService implements HttpInterceptor{
   constructor(private snackBar: MatSnackBar) { }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(tap((success) => {
+    return next.handle(req)
+      .pipe( tap((success) => {
       if (success instanceof HttpResponse) {
-        const urlLastSegment = success.url.split('/').pop();
-      
-        let message: string = null;
-        switch (urlLastSegment) {
-          case 'login':
-            message = `Hello ${success.body.user.name}!`;
-            break;
-          case 'changeRole':
-            message = `Successfully changed user role`;
-            break;
-          case 'postWorkout':
-          case 'addExercise':
-            message = success['body'].message;
-            break;
-        }
-    
-        if (message) {
-          this.snackBar.open(message, '', {
-            duration: 4000
-          });
-        }
+        this.setToasterIfNeeded(success);
       }
-    }), catchError((err) => {
+    }), catchError(err => {
       if (err['error']) {
         this.snackBar.open(err.error.message, 'Undo', {
           duration: 4000,
         });
       }
-      throw err;
+      return throwError(err);
     }));
+  }
+
+  private setToasterIfNeeded(success) {
+    const urlLastSegment = success.url.split('/').pop();
+    let message: string = null;
+    switch (urlLastSegment) {
+      case 'login':
+        message = `Hello ${success.body.user.name}!`;
+        break;
+      case 'changeRole':
+        message = `Successfully changed user role`;
+        break;
+      case 'postWorkout':
+      case 'addExercise':
+        message = success['body'].message;
+        break;
+    }
+  
+    if (message) {
+      this.snackBar.open(message, '', {
+        duration: 4000
+      });
+    }
   }
 }
